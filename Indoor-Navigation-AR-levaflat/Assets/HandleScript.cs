5d18eef82961c45258ea305493c21babeec6a00a
leva1222 <51375198+leva1222@users.noreply.github.com>
2020-05-02 18:17:37 +0400

    Lef

---
+++ Indoor-Navigation-AR-levaflat/Assets/HandleScript.cs
@@ -0,0 +1,46 @@
+﻿using UnityEngine;
+
+using UnityEngine.EventSystems;
+
+public class HandleScript : MonoBehaviour
+
+{
+    public Camera map;
+    // здесь будет ссылка на компонент Transform зеленой панели.
+
+
+    private readonly float speed = 1f;
+    private bool move = false;
+    private Vector3 target;
+    private Vector2 startPos;
+
+
+
+    private void Update()
+    {
+
+        if (Input.touchCount == 1)
+        {
+            var touch = Input.GetTouch(0);
+
+            switch (touch.phase)
+            {
+                case TouchPhase.Began:
+                    startPos = Camera.main.ScreenToWorldPoint(touch.position);
+                    break;
+
+                case TouchPhase.Moved:
+                    var dir = Camera.main.ScreenToWorldPoint(touch.position);
+                    dir.x -= startPos.x;
+                    dir.z -= startPos.y;
+                    dir.y = 0;
+                    var pos = map.transform.position + dir;
+                    map.transform.position = Vector3.Lerp(map.transform.position, pos, Time.deltaTime * speed);
+                    break;
+            }
+        }
+
+    }
+
+
+}
---
+++ Indoor-Navigation-AR-levaflat/Assets/HandleZoom.cs
@@ -0,0 +1,82 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HandleZoom : MonoBehaviour
+{
+    public float sensitivity;
+
+    Vector2 f0start;
+
+    Vector2 f1start;
+    private bool drag = false;
+    // масштабируем
+
+    // экранные координаты начальной точки касания
+    private Vector3 initialTouchPosition;
+    // мировые координаты камеры при инициировании
+    // перемещения/масштабирования
+    private Vector3 initialCameraPosition;
+    void Update()
+
+    {
+
+        if (Input.touchCount == 0)
+
+        {
+
+            f0start = Vector2.zero;
+
+            f1start = Vector2.zero;
+
+        }
+        if (Input.touchCount == 2) Zoom();
+        if (Input.touchCount == 1) Swap();
+
+    }
+
+    void Swap()
+    {
+        Touch touch0 = Input.GetTouch(0);
+        if (!drag)
+        {
+            initialTouchPosition = touch0.position;
+            initialCameraPosition = transform.position;
+
+            drag = true;
+        }
+        else
+        {
+            Vector2 delta = touch0.position;
+            delta.x -= initialTouchPosition.x;
+            delta.y -= initialTouchPosition.y;
+            drag = false;
+            Vector3 newPos = initialCameraPosition;
+            newPos.x -= delta.x;
+            newPos.z -= delta.y;
+            transform.position =  Vector3.MoveTowards(transform.position, newPos, 10f * Time.deltaTime * Mathf.Sign(Vector2.Distance(new Vector2(initialTouchPosition.x, initialTouchPosition.y), delta)));
+        }
+    }
+    void Zoom()
+
+    {
+
+        if (f0start == Vector2.zero && f1start == Vector2.zero)
+
+        {
+
+            f0start = Input.GetTouch(0).position;
+
+            f1start = Input.GetTouch(1).position;
+
+        }
+
+        Vector2 f0position = Input.GetTouch(0).position;
+
+        Vector2 f1position = Input.GetTouch(1).position;
+
+        float dir = Mathf.Sign(Vector2.Distance(f1start, f0start) - Vector2.Distance(f0position, f1position));
+        var target = transform.position + transform.forward;
+        transform.position = Vector3.MoveTowards(transform.position, target, dir * sensitivity * Time.deltaTime * Vector3.Distance(f0position, f1position));
+    }
+}
